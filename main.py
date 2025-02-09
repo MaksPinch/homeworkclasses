eefa72a9c5c3e5f395c69babb04dff53e51f6b17
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2025-02-09 11:16:36 +0100

    exercise 4 is done

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ class Student:
         self.grades = {}
 
     def rate_lecturer(self, lecturer, course, grade):
-        if  isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
             if 1 <= grade <= 10:
                 if course in lecturer.grades:
                     lecturer.grades[course] += [grade]
@@ -33,12 +34,10 @@ class Student:
     def __str__(self):
         avg_grade = self.average_grade()
 
-
         if self.courses_in_progress:
             courses_in_progress = ", ".join(self.courses_in_progress)
         else:
             courses_in_progress = "Нет"
-
 
         if self.finished_courses:
             finished_courses = ", ".join(self.finished_courses)
@@ -108,18 +107,38 @@ class Lecturer(Mentor):
         return self.average_grade() == other.average_grade()
 
 
+def average_students_grade(course, students):
+    total_grades = []
+    for student in students:
+        if course in student.grades:
+            total_grades.extend(student.grades[course])
+
+        if len(total_grades) > 0:
+            return round(sum(total_grades) / len(total_grades), 1)
+        else:
+            return 0
+
+
+def average_lecturers_grade(course, lecturers):
+    total_grades = []
+    for lecturer in lecturers:
+        if course in lecturer.grades:
+            total_grades.extend(lecturer.grades[course])
+
+        if len(total_grades) > 0:
+            return round(sum(total_grades) / len(total_grades), 1)
+        else:
+            return 0
+
 
 best_student = Student('Ruoy', 'Eman', 'male')
 best_student.courses_in_progress += ['Python']
-
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 
 cool_lecturer = Lecturer('Alexandr', 'An')
 cool_lecturer.courses_attached += ['Python']
-
-
 
 # run the methods
 
@@ -148,8 +167,6 @@ another_reviewer.courses_attached += ['Java']
 another_lecturer = Lecturer('The', 'Rock')
 another_lecturer.courses_attached += ['Java']
 
-
-
 result3 = another_reviewer.rate_hw(another_student, 'Python', 5)
 result4 = another_student.rate_lecturer(another_lecturer, 'Python', 8)
 
@@ -161,7 +178,6 @@ print()
 # exercise 3
 print(" ! 3 задание !")
 
-
 student_1 = Student('Ruoy', 'Eman', 'male')
 student_1.courses_in_progress += ['Python', 'Git']
 student_1.finished_courses += ['Введение в программирование']
@@ -170,29 +186,24 @@ student_2 = Student('Alice', 'J', 'female')
 student_2.courses_in_progress += ['Java']
 student_2.finished_courses += ['C++']
 
-
 lecturer_1 = Lecturer('John', 'D')
 lecturer_1.courses_attached += ['Python']
 
 lecturer_2 = Lecturer('Sarah', 'C')
 lecturer_2.courses_attached += ['Java']
 
-
 reviewer_1 = Reviewer('Some', 'Buddy')
 reviewer_1.courses_attached += ['Python']
-
 
 reviewer_1.rate_hw(student_1, 'Python', 10)
 reviewer_1.rate_hw(student_1, 'Python', 9)
 reviewer_1.rate_hw(student_1, 'Python', 8)
-
 
 student_1.rate_lecturer(lecturer_1, 'Python', 10)
 student_1.rate_lecturer(lecturer_1, 'Python', 9)
 
 student_2.rate_lecturer(lecturer_2, 'Java', 8)
 student_2.rate_lecturer(lecturer_2, 'Java', 7)
-
 
 print(reviewer_1)
 print()
@@ -203,10 +214,17 @@ print(student_1)
 print(student_2)
 print()
 
-
 print(f"{student_1.name} лучше {student_2.name}? {student_1 > student_2}")
 print(f"{student_1.name} и {student_2.name} равны по оценкам? {student_1 == student_2}")
 
-
 print(f"{lecturer_1.name} лучше {lecturer_2.name}? {lecturer_1 > lecturer_2}")
 print(f"{lecturer_1.name} и {lecturer_2.name} равны по оценкам? {lecturer_1 == lecturer_2}")
+
+print()
+print(' ! задание 4 !')
+
+students = [student_1, student_2]
+lecturers = [lecturer_1, lecturer_2]
+
+print(f"Средняя оценка студентов на курсе Python: {average_students_grade('Python', students)}")
+print(f"Средняя оценка лекторов на курсе Python: {average_lecturers_grade('Python', lecturers)}")
